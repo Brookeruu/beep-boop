d7b9256945fa996704472298720591db83032802
Brooke Perry <perrysbrooke@gmail.com>
2018-10-26 11:53:19 -0700

    try to add modulos to js, don't know if its working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 // Business Logic
 var arrayOfNumbers1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 var arrayOfExceptions = ["Beep!","Boop!","Sorry, I cannot do that!", "Invalid Number!"];
-var emptyStringForResult = "";
+var emptyArray = [];
 
-function makeAstringOfNummbers(originalInput) {
+function makeAstringOfNummbers(number) {
+
   for (var i = 0; i < arrayOfNumbers1.length; i ++);
-    if (originalInput < 0 || i > 15 ) {
-      return emptyStringForResult += arrayOfExceptions[3]
-      console.log(emptyStringForResult);
+    if (number > 0 && number < arrayOfNumbers1.length) {
+      if (number % 3 === 0) {
+        emptyArray.push(arrayOfExceptions[3])
+        return makeAstringOfNummbers(number);
+      }else if (number === 0 || number % 10 === 0) {
+        emptyArray.push(arrayOfExceptions[1])
+        return makeAstringOfNummbers(number)
+      }
     }
-    else if (originalInput === 0) {
-      return emptyStringForResult += arrayOfExceptions[0];
-    }
-    // else if ( )
-
-}
+return emptyArray;
+});
 
 
 
@@ -24,10 +26,11 @@ $(document).ready(function() {
   $(".form-group").submit(function(event) {
     event.preventDefault();
 
-  var originalUserInput = parseInt($("#numberInput").val());
-  console.log(originalUserInput);
+  var inputNumber = parseInt($("#numberInput").val());
+  // console.log(originalUserInput);
 
-  var resultToShowUser = makeAstringOfNummbers(originalUserInput);
+  var resultToShowUser = makeAstringOfNummbers(inputNumber);
+  console.log(makeAstringOfNummbers);
 
   });
 });
