1821414086b5395a004bcaf09847b4af866a212e
Brooke Perry <perrysbrooke@gmail.com>
2018-10-29 20:12:31 -0700

    remove unnecessary function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
 // Business Logic
 
+function convertNumberToArray(number) { //number is inputNumber as argument
+  var arrayOfNumbers = new Array(number + 1); //new Array() creates array with empty spaces to fill
+  var count = 0; //each empty space receives count
+  for (var i = 0; i < arrayOfNumbers.length; i++) {
+    arrayOfNumbers[i] = count;
+    count += 1;
+  }
+  return arrayOfNumbers; //array with numbers in each empty space, up to # of inputNumber;
+}
+
+
+// function beepBoopSorry(array) {
+//   var beepBoopSorryArray = []
+//   for (i=0; i<array.length; i++) {
+//     if (i % 3 === 0 && i !== 0) {
+//       array[i] = " I'm sorry, Dave. I'm afraid I can't do that. "
+//     } else if (array[i].includes(1)) {
+//       array[1] = " Boop! ";
+//     } else if (array[i].includes(0)) {
+//       array[i] = " Beep! ";
+//     }
+//   }
+//   return beepBoopSorryArray;
+//   console.log(beepBoopSorryArray);
+// }
+//
+//
+
+
+
+
 function beep(array) { //argument will be arrayOfNumbers
   var beepArray = array;
   for (var i = 0; i < beepArray.length; i++) {
@@ -15,6 +46,8 @@ function sorry(array) {
   for (var i = 0; i < sorryArray.length; i++) {
     if (i % 3 === 0 && i !== 0) {
       sorryArray[i] = " I'm sorry, Dave. I'm afraid I can't do that. ";
+    } else if (array[i].includes(0)) {
+      sorryArray[i] = " Beep! ";
     }
   }
   return sorryArray;
@@ -30,19 +63,7 @@ function boopMod1(array) {
   return boopMod1Array;
 }
 
-function convertNumberToArray(number) { //number is inputNumber as argument
-  if (number < 0 || number > 100) {
-    alert("Invalid Number: returning 10");
-    number = 10;
-  }
-  var arrayOfNumbers = new Array(number + 1); //new Array() creates array with empty spaces to fill
-  var count = 0; //each empty space receives count
-  for (var i = 0; i < arrayOfNumbers.length; i++) {
-    arrayOfNumbers[i] = count;
-    count += 1;
-  }
-  return arrayOfNumbers; //array with numbers in each empty space, up to # of inputNumber;
-}
+
 
 
 // User Interface
@@ -54,10 +75,15 @@ $(document).ready(function() {
     var inputNumber = parseInt($("#numberInput").val());
 
     var convertedArray = convertNumberToArray(inputNumber);
+    console.log(convertedArray);
 
-    var beepTest = beep(convertedArray);
+    // var newArray = beepBoopSorry(convertedArray);
+    // console.log(newArray);
+    //
+    // var beepTest = beep(convertedArray);
 
-    var sorryTest = sorry(beepTest);
+    var sorryTest = sorry(convertedArray);
+    console.log(sorryTest);
 
     var boopTest = boopMod1(sorryTest);
 
